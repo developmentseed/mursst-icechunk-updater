a725b9eec2eedd2b6e8718b584a11e5a12ec11f2
Julius Busecke <you@example.com>
2025-08-14 22:28:22 +0000

    Lighten core dependencies and dont export dev deps

--- scripts/sync_requirements.sh
+++ scripts/sync_requirements.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-uv export --format=requirements.txt --no-hashes --no-annotate --no-header --no-emit-workspace > cdk/lambda/requirements.txt
+uv export --format=requirements.txt --no-hashes --no-annotate --no-header --no-emit-workspace --no-group dev> cdk/lambda/requirements.txt
