0d8fdf8a9de014ad1ba770a015239c0fcd266f91
Julius Busecke <you@example.com>
2025-09-04 20:11:40 +0000

    slim down lambda code

--- scripts/sync_requirements.sh
+++ scripts/sync_requirements.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-uv export --format=requirements.txt --no-hashes --no-annotate --no-header --no-emit-workspace --no-group dev> cdk/aws_lambda/requirements.txt
+uv export --format=requirements.txt --no-hashes --no-annotate --no-header --no-emit-workspace --no-group deploy --no-group dev --no-group notebook> cdk/aws_lambda/requirements.txt
 uv export --format=requirements.txt --no-hashes --no-annotate --no-header --no-emit-workspace --only-group deploy> cdk/requirements.txt
